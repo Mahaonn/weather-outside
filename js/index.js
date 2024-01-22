291acea55137e1c3ce51d128fb1d7eaa5dbacf6a
Yevheniia Kovzikova <144259600+Mahaonn@users.noreply.github.com>
2024-01-22 20:46:09 +0100

    Added elements: description, humidity, wind and current icon.
    replaced in the div class="current-temperature spans

--- js/index.js
+++ js/index.js
@@ -2,8 +2,18 @@ function displayTemperature(response) {
   let temperatureElement = document.querySelector("#current-temperature");
   let temperature = Math.round(response.data.temperature.current);
   let cityElement = document.querySelector("#current-city");
+  let descriptionElement = document.querySelector("#weather-description");
+  let humidityElement = document.querySelector("#current-humidity");
+  let windElement = document.querySelector("#wind-speed");
+  let windSpeed = response.data.wind.speed;
+  let iconElement = document.querySelector("#weather-icon");
+
   cityElement.innerHTML = response.data.city;
   temperatureElement.innerHTML = temperature;
+  descriptionElement.innerHTML = response.data.condition.description;
+  humidityElement.innerHTML = response.data.temperature.humidity;
+  windElement.innerHTML = Math.round(windSpeed * 10) / 10;
+  iconElement.innerHTML = `<img src="${response.data.condition.icon_url}" class="weather-icon" />`;
 }
 
 function searchCity(city) {
@@ -21,7 +31,6 @@ function handleSearchSubmit(event) {
 function formatDate(date) {
   let minutes = date.getMinutes();
   let hours = date.getHours();
-  let day = date.getDay();
 
   if (minutes < 10) {
     minutes = `0${minutes}`;
@@ -41,14 +50,14 @@ function formatDate(date) {
     "Saturday",
   ];
 
-  let formattedDay = days[day];
-  return `${formattedDay} ${hours}:${minutes}`;
+  let day = days[date.getDay()];
+  return `${day} ${hours}:${minutes}`;
 }
 
-let currentDateELement = document.querySelector("#current-date");
+let currentTimeELement = document.querySelector("#current-time");
 let currentDate = new Date();
 
-currentDateELement.innerHTML = formatDate(currentDate);
+currentTimeELement.innerHTML = formatDate(currentDate);
 
 let searchForm = document.querySelector("#search-form");
 searchForm.addEventListener("submit", handleSearchSubmit);
